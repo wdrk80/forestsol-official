ee8b32db16dc218f0a1048f5d84e0fd95da1b801
wdrk80 <wdrk80@gmail.com>
2026-07-31 12:38:16 +0900

    Enable ForestFarm application submissions

--- assets/application-config.js
+++ assets/application-config.js
@@ -5,5 +5,5 @@
    * ヴァルト君のHTTPS受付URLだけを設定します。
    * Botトークンや秘密情報は、この公開ファイルへ絶対に書かないでください。
    */
-  window.FORESTFARM_APPLICATION_ENDPOINT = "";
+  window.FORESTFARM_APPLICATION_ENDPOINT = "https://api.forestsol.jp/api/forestfarm/applications";
 })();
